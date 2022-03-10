44206cc17722a117b2b83ba3984a6178c248726c
danheon kim <eksgjs3847@naver.com>
2022-03-10 21:55:13 +0900

    Fix a bug about menu layouts & Add a sticky menu interaction

--- js/main.js
+++ js/main.js
@@ -140,6 +140,14 @@
   }
   setCanvasImages();
 
+  function checkMenu() {
+    if (yOffset > 44) {
+      document.body.classList.add("local-nav-sticky");
+    } else {
+      document.body.classList.remove("local-nav-sticky");
+    }
+  }
+
   function setLayout() {
     // 각 스크롤 섹션의 높이 세팅
     for (let i = 0; i < sceneInfo.length; i++) {
@@ -346,6 +354,7 @@
   window.addEventListener('scroll', () => {
     yOffset = window.pageYOffset;
     scrollLoop();
+    checkMenu();
   })
   window.addEventListener('load', () => {
     setLayout();
